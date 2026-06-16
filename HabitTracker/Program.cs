007b1f147875879d36cee4a24d99dc3d09a09b0d
klaudia021 <61354324+klaudia021@users.noreply.github.com>
2026-06-16 11:14:13 +0200

    feat: setup sqlite connection

--- HabitTracker/Program.cs
+++ HabitTracker/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Microsoft.Data.Sqlite;
+
+string connectionString = @"Data Source=Habit-Tracker.db";
+
+using (var connection = new SqliteConnection(connectionString))
+{
+    connection.Open();
+
+    SqliteCommand command = connection.CreateCommand();
+    command.CommandText = @"CREATE TABLE IF NOT EXISTS DrinkingWater (
+                            DrinkingWaterId INTEGER PRIMARY KEY AUTOINCREMENT,
+                            Date TEXT,
+                            Quantity INTEGER
+                        )";
+
+    command.ExecuteNonQuery();
+    
+    connection.Close();
+}
