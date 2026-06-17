d48824495d481521c914e4e3bd722d516fd13109
klaudia021 <61354324+klaudia021@users.noreply.github.com>
2026-06-17 12:38:35 +0200

    feat: add test for listmenu

--- HabitTrackerTests/MenuServiceTests.cs
+++ HabitTrackerTests/MenuServiceTests.cs
@@ -1,4 +1,6 @@
-﻿namespace HabitTrackerTests;
+﻿using Services;
+
+namespace HabitTrackerTests;
 
 public class MenuServiceTests
 {
@@ -24,5 +26,27 @@ public class MenuServiceTests
 
         // Assert
         Assert.That(menuNumber, Is.EqualTo(expectedInt));
+    }
+
+    [Test]
+    public void ListMenu_ShouldListMenuOptions()
+    {
+        // Arrange
+        const string ViewAllText =      "1 - View all records";
+        const string InsertRecordText = "2 - Insert a record";
+        const string DeleteRecordText = "3 - Delete a record";
+        const string UpdateRecordText = "4 - Update a record";
+
+        using var output = new StringWriter();
+        Console.SetOut(output);
+
+        // Act
+        MenuService.ListMenu();
+
+        // Assert
+        Assert.That(output.ToString(), Does.Contain(ViewAllText));
+        Assert.That(output.ToString(), Does.Contain(InsertRecordText));
+        Assert.That(output.ToString(), Does.Contain(DeleteRecordText));
+        Assert.That(output.ToString(), Does.Contain(UpdateRecordText));
     }
 }
